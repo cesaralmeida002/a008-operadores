f1015cd3390707fe4535c7209f46830a7a01120e
cesaralmeida002 <cesaralmeida002@gmail.com>
2022-08-30 15:54:35 -0300

    Exercicios 1-2-3 realizados com sucesso

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+const operacao1 = (5 > 20 && 5 < 2)
+console.log("A operação é:", operacao1)
+
+const operacao2 = (5 === 5 || 5 === "5")
+console.log("A operação é:", operacao2)
+
+const operacao3 = !(20 > 50)
+console.log("A operação é:", operacao3)
+
+const operacao4 = !(20 > 50 || 50 >70)
+console.log("A operação e:", operacao4) 
